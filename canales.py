257998a0e821e09bb7dfc853b28c408094bd6df3
CRYPTO-YISUS <57347258+CRYPTO-YISUS@users.noreply.github.com>
2022-07-11 16:58:48 -0500

    Add files via upload

--- canales.py
+++ canales.py
@@ -14,18 +14,18 @@ def reqweb(url):
 
 def Wordlist():
 	words = []
-	with open("C:/Users/JesusV/Documents/python/mx.m3u") as file:
+	with open("C:/Users/IsMerk/Desktop/mx.m3u") as file:
 		for lines in file:
 			words.extend(lines.split())
 		return words
 
 def Zip_():
-	if os.path.isfile("C:/Users/JesusV/Desktop/LATINOMX/LATINOMX.zip"):
-		os.remove("C:/Users/JesusV/Desktop/LATINOMX/LATINOMX.zip")
+	if os.path.isfile("C:/Users/IsMerk/Desktop/LATINOMX.zip"):
+		os.remove("C:/Users/IsMerk/Desktop/LATINOMX.zip")
 		return Zip_()
 	else:
 		print("CREANDO ZIP...")
-		file_zip = shutil.make_archive("C:/Users/JesusV/Desktop/LATINOMX","zip","C:/Users/JesusV/Desktop/LATINOMX")
+		file_zip = shutil.make_archive("C:/Users/IsMerk/Desktop/LATINOMX","zip","C:/Users/IsMerk/Desktop/LATINOMX")
 		print("ZIP CREADO.")
 
 
@@ -36,37 +36,42 @@ def main():
 	temp = []
 	urls =['http://192.168.1.61/','http://192.168.1.62/','http://192.168.1.63/','http://192.168.1.64/','http://192.168.1.65/','http://192.168.1.66/','http://192.168.1.67/','http://192.168.1.68/','http://192.168.1.69/']
 
-	if os.path.isfile("C:/Users/JesusV/Documents/python/mx.m3u"):
+	if os.path.isfile("C:/Users/IsMerk/Desktop/mx.m3u"):
 		print("\t\t" + "***************************************************")
 		print("\t\t" + "*" + "\t" + "\t" + "COFIGURATION MAKER" + "\t\t  " + "*")
 		print("\t\t" + "***************************************************")
 		print ("ARCHIVO ENCONTRADO\nBORRANDO...")
-		os.remove("C:/Users/JesusV/Documents/python/mx.m3u")
+		os.remove("C:/Users/IsMerk/Desktop/mx.m3u")
 		return main()
 	else:
 		print("\n"+"DESCARGANDO...")
-		url = "http://regioplay.xyz/s21/mx.m3u"
-		wget.download(url,'c:/Users/JesusV/Documents/python')
+		url = "http://regioplay.xyz/j21/mx.m3u"
+		wget.download(url,"C:/Users/IsMerk/Desktop")
 		print("\n" + "DESCARGA COMPLETA.\n")
 		print("CREANDO CONFIGURACION...")
 		w = Wordlist()
-		form = open("C:/Users/JesusV/Desktop/LATINOMX/components/Config.brs","w")
-		total=len(w)
+		form = open("C:/Users/IsMerk/Desktop/LATINOMX/components/Config.brs","w")
 
-		for url in w:
-			if "SR_REGIO" in url and "tp.php" in url:
-				urlchannel.append(url)
-
-		for z in range(0,total):
-			if "NACIONALES" in w[z]:
-				y=z
+		for z in range(0,len(w)):
+			if "EVENTOS" in w[z]:
+				y=z-10
+			if "Vix" in w[z]:
+				total=z
 				break
-				
+
+		for z in range(y+10, total):
+			if "logo" in w[z]:
+				pass
+			elif "http" in w[z]:
+				urlchannel.append(w[z])
+
 		for i in range(y,total):
-			if "-1" in w[i]:
-				for j in range(1,4):
+			if "#EXTINF:-1" in w[i]:
+				for j in range(3,10):
 					if "http" in w[i+j]:
 						break
+					elif "tvg-id" in w[i+j] or '"' in w[i+j]:
+						pass
 					else:
 						temp.append(w[i+j])
 				temp.append('*')
@@ -74,11 +79,24 @@ def main():
 		namelong = ' '.join(temp)
 		namechannel = namelong.split(sep='*')
 
-		namechannel.pop()
-
+		for m in range(len(namechannel)):
+			for n in range(len(namechannel)):
+				if namechannel[n] == ' ':
+					namechannel.pop(n)
+					break
+		m,n=0,0
+		for m in range(len(urlchannel)):
+			for n in range(len(urlchannel)):
+				if ".m3u8" in urlchannel[n]:
+					pass
+				else:
+					urlchannel.pop(n)
+					namechannel.pop(n)
+					break
+		
 		form.write("Function loadConfig() as Object\narr=[\n")
 		form.close()
-		form1 = open("C:/Users/JesusV/Desktop/LATINOMX/components/Config.brs","a")
+		form1 = open("C:/Users/IsMerk/Desktop/LATINOMX/components/Config.brs","a")
 
 		for h in range(len(urlchannel)):
 			form1.write("\n{\n\tTitle:\"" + namechannel[h] + "\"\n\tstreamFormat: \"hls\"" + "\n\tLogo: \" \"" + "\n\tStream: \"" + urlchannel[h] + "\"\n}")
